1503fddfbb99eec2da8983d1c1bc43876b3b887d
Mysterio-O <skrabbi.019@gmail.com>
2025-06-03 16:47:29 +0600

    force logout a user when token doesnt match with the email or if the token is unavailable

--- src/hooks/useAxiosSecret.jsx
+++ src/hooks/useAxiosSecret.jsx
@@ -8,11 +8,28 @@ const axiosInstance = axios.create({
 
 const useAxiosSecret = () => {
 
-    const { user } = useAuth();
+    const { user, logOutUser } = useAuth();
 
+    // request interceptor
     axiosInstance.interceptors.request.use(config => {
         config.headers.authorization = `Bearer ${user?.accessToken}`
         return config
+    })
+
+    // response interceptor
+    axiosInstance.interceptors.response.use(response => {
+        return response;
+    }, error => {
+        // console.log(error);
+        if (error.status === 401 || error.status === 403) {
+            logOutUser().then(() => {
+                console.log('user logged out for 401 or 403 status');
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+
+        return Promise.reject(error);
     })
 
     return axiosInstance
